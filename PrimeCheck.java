77e16144c90ca1abc97a10afcc9c8f01eeab8e1b
kabilan6943 <kabilan6943@gmail.com>
2025-09-18 15:53:13 +0530

    checcking prime number

---
+++ PrimeCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n <= 1) {
+            System.out.println("Not Prime");
+        
+        }
+        boolean prime = true;
+        for(int i=2;i<=Math.sqrt(n);i++) {
+            if(n % i == 0) { prime=false; break; }
+        }
+        System.out.println(prime ? "Prime" : "Not Prime");
+        sc.close();
+    }
+}
