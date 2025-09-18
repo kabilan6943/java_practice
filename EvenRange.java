c6138d94cf8ba0d4adba762c61673ec630fbe547
kabilan6943 <kabilan6943@gmail.com>
2025-09-18 15:42:09 +0530

    even range

---
+++ EvenRange.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class EvenRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt(), end = sc.nextInt();
+        for(int i=start;i<=end;i++) {
+            if(i % 2 == 0) System.out.print(i + " ");
+        }
+        sc.close();
+        
+    }
+}
